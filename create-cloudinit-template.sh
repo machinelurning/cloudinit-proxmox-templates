8213b93261f7d049daaab5bdd3dd138b1ef6ec19
IK Manding <github@ianmanding.net>
2024-01-15 18:22:30 +0800

    initial commit

---
+++ create-cloudinit-template.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+imageURL=https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img
+imageName="focal-server-cloudimg-amd64.img"
+volumeName="local-lvm"
+virtualMachineId="9000"
+templateName="ubuntu-cloudinit"
+tmp_cores="2"
+tmp_memory="2048"
+rootPasswd="password"
+cpuTypeRequired="host"
+
+apt update
+apt install libguestfs-tools -y
+rm *.img
+wget -O $imageName $imageURL
+qm destroy $virtualMachineId
+virt-customize -a $imageName --install qemu-guest-agent
+virt-customize -a $imageName --root-password password:$rootPasswd
+qm create $virtualMachineId --name $templateName --memory $tmp_memory --cores $tmp_cores --net0 virtio,bridge=vmbr0
+qm importdisk $virtualMachineId $imageName $volumeName
+qm set $virtualMachineId --scsihw virtio-scsi-pci --scsi0 $volumeName:vm-$virtualMachineId-disk-0
+qm set $virtualMachineId --boot c --bootdisk scsi0
+qm set $virtualMachineId --ide2 $volumeName:cloudinit
+qm set $virtualMachineId --serial0 socket --vga serial0
+qm set $virtualMachineId --ipconfig0 ip=dhcp
+qm set $virtualMachineId --cpu cputype=$cpuTypeRequired
+qm template $virtualMachineId
